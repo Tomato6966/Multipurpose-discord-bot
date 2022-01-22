41bc2c85483a47ec0ff7728c60ae20e4c62fdb62
Tomato6966 <chris.pre03@gmail.com>
2022-01-22 13:54:11 +0100

    Update loaddb.js

--- handlers/loaddb.js
+++ handlers/loaddb.js
@@ -85,72 +85,27 @@ module.exports = client => {
     });
 
 
-    client.autosupport1 = new Enmap({
-        name: "autosupport",
-        dataDir: "./databases/menuticket"
-    });
-    client.autosupport2 = new Enmap({
-        name: "autosupport2",
-        dataDir: "./databases/menuticket"
-    });
-    client.autosupport3 = new Enmap({
-        name: "autosupport3",
-        dataDir: "./databases/menuticket"
-    });
-    client.menuticket1 = new Enmap({
-        name: "menuticket",
-        dataDir: "./databases/menuticket"
-    });
-    client.menuticket2 = new Enmap({
-        name: "menuticket2",
-        dataDir: "./databases/menuticket"
-    });
-    client.menuticket3 = new Enmap({
-        name: "menuticket3",
-        dataDir: "./databases/menuticket"
-    });
-    client.menuapply1 = new Enmap({
-        name: "menuapply",
-        dataDir: "./databases/menuticket"
-    });
-    client.menuapply2 = new Enmap({
-        name: "menuapply2",
-        dataDir: "./databases/menuticket"
-    });
-    client.menuapply3 = new Enmap({
-        name: "menuapply3",
-        dataDir: "./databases/menuticket"
-    });
-
-    client.apply = new Enmap({
-        name: "apply",
-        dataDir: "./databases/apply"
-    });
-    client.apply2 = new Enmap({
-        name: "apply2",
-        dataDir: "./databases/apply2"
-    });
-    client.apply3 = new Enmap({
-        name: "apply3",
-        dataDir: "./databases/apply3"
-    });
-    client.apply4 = new Enmap({
-        name: "apply4",
-        dataDir: "./databases/apply4"
-    });
-    client.apply5 = new Enmap({
-        name: "apply5",
-        dataDir: "./databases/apply5"
-    });
-    require("fs").readdirSync("./handlers/applies").forEach(file => {
-        let filename = String(file).replace(".js", "")
-        client[`${filename}`] = new Enmap({
-            name: `${filename}`,
-            dataDir: "./databases/otherapplies"
+    for (let i = 1; i <= 3; i++) {
+        client[`autosupport${i}`] = new Enmap({
+            name: `autosupport${i != 1 ? i : ""}`,
+            dataDir: `./databases/menuticket`
         });
-    });
-
-
+        client[`menuticket${i}`] = new Enmap({
+            name: `menuticket${i != 1 ? i : ""}`,
+            dataDir: `./databases/menuticket`
+        });
+        client[`menuapply${i}`] = new Enmap({
+            name: `menuapply${i != 1 ? i : ""}`,
+            dataDir: `./databases/menuticket`
+        });
+    }
+    for (let i = 1; i <= 25; i++) {
+        client[`apply${i != 1 ? i : ""}`] = new Enmap({
+            name: `apply${i != 1 ? i : ""}`,
+            dataDir: `./databases/apply${i == 1 ? "" : i == 2 ? "2" : i == 3 ? "3" : i == 4 ? "4" : "5"}`
+        });
+    }
+    
 
     client.points = new Enmap({
         name: "points",
@@ -188,20 +143,9 @@ module.exports = client => {
         dataDir: "./databases/keyword"
     });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    /**
+     * ENSURE DATA
+     */
     client.premium.ensure("premiumlist", {
         list: [
             /*{
@@ -222,84 +166,9 @@ module.exports = client => {
     client.afkDB.ensure("REMIND", {
         REMIND: []
     });
-    client.setups.ensure("TICKETS", {
-        tickets: [],
-        tickets1: [],
-        tickets2: [],
-        tickets3: [],
-        tickets4: [],
-        tickets5: [],
-        tickets6: [],
-        tickets7: [],
-        tickets8: [],
-        tickets9: [],
-        tickets10: [],
-        tickets11: [],
-        tickets12: [],
-        tickets13: [],
-        tickets14: [],
-        tickets15: [],
-        tickets16: [],
-        tickets17: [],
-        tickets18: [],
-        tickets19: [],
-        tickets20: [],
-        tickets21: [],
-        tickets22: [],
-        tickets23: [],
-        tickets24: [],
-        tickets25: [],
-        applytickets1: [],
-        applytickets2: [],
-        applytickets3: [],
-        applytickets4: [],
-        applytickets5: [],
-        applytickets6: [],
-        applytickets7: [],
-        applytickets8: [],
-        applytickets9: [],
-        applytickets10: [],
-        applytickets11: [],
-        applytickets12: [],
-        applytickets13: [],
-        applytickets14: [],
-        applytickets15: [],
-        applytickets16: [],
-        applytickets17: [],
-        applytickets18: [],
-        applytickets19: [],
-        applytickets20: [],
-        applytickets21: [],
-        applytickets22: [],
-        applytickets23: [],
-        applytickets24: [],
-        applytickets25: [],
-        menutickets: [],
-        menutickets0: [],
-        menutickets1: [],
-        menutickets2: [],
-        menutickets3: [],
-        menutickets4: [],
-        menutickets5: [],
-        menutickets6: [],
-        menutickets7: [],
-        menutickets8: [],
-        menutickets9: [],
-        menutickets10: [],
-        menutickets11: [],
-        menutickets12: [],
-        menutickets13: [],
-        menutickets14: [],
-        menutickets15: [],
-        menutickets16: [],
-        menutickets17: [],
-        menutickets18: [],
-        menutickets19: [],
-        menutickets20: [],
-        menutickets21: [],
-        menutickets22: [],
-        menutickets23: [],
-        menutickets24: [],
-        menutickets25: [],
-    });
+    let ensureObject = {};
+    for(let i = 0; i<=26; i++) ensureObject[`tickets${i != 26 ? i : ""}`] = [];
+    for(let i = 0; i<=26; i++) ensureObject[`applytickets${i != 26 ? i : ""}`] = [];
+    for(let i = 0; i<=26; i++) ensureObject[`menutickets${i != 26 ? i : ""}`] = [];
+    client.setups.ensure("TICKETS", ensureObject);
 }
