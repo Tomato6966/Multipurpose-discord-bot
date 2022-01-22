7fa027a5ec85a8bc9f6953ed9f5b0468f2e28363
Tomato6966 <chris.pre03@gmail.com>
2022-01-22 12:32:31 +0100

    Add files via upload

---
+++ commands/🕹️ Fun/cry.js
@@ -0,0 +1,64 @@
+﻿const Discord = require("discord.js");
+const {MessageEmbed, MessageAttachment} = require("discord.js");
+const config = require(`${process.cwd()}/botconfig/config.json`);
+const canvacord = require("canvacord");
+var ee = require(`${process.cwd()}/botconfig/embed.json`);
+const request = require("request");
+const emoji = require(`${process.cwd()}/botconfig/emojis.json`);
+module.exports = {
+  name: "cry",
+  aliases: [""],
+  category: "🕹️ Fun",
+  description: "IMAGE CMD",
+  usage: "cry <TEXT>",
+  type: "text",
+  run: async (client, message, args, cmduser, text, prefix) => {
+    
+    let es = client.settings.get(message.guild.id, "embed");let ls = client.settings.get(message.guild.id, "language")
+        if(!client.settings.get(message.guild.id, "FUN")){
+          return message.reply({embeds : [new MessageEmbed()
+            .setColor(es.wrongcolor)
+            .setFooter(client.getFooter(es))
+            .setTitle(client.la[ls].common.disabled.title)
+            .setDescription(require(`${process.cwd()}/handlers/functions`).handlemsg(client.la[ls].common.disabled.description, {prefix: prefix}))
+          ]});
+        }
+      //send loading message
+      var tempmsg = await message.reply({embeds : [new MessageEmbed()
+        .setColor(ee.color)
+        .setAuthor( 'Getting Image Data..', 'https://images-ext-1.discordapp.net/external/ANU162U1fDdmQhim_BcbQ3lf4dLaIQl7p0HcqzD5wJA/https/cdn.discordapp.com/emojis/756773010123522058.gif')
+      ]});
+      //get the additional text
+      var text = args.join(" ");
+      //If no text added, return error
+      if(!text) return tempmsg.edit({embeds : [tempmsg.embeds[0]
+        .setTitle(eval(client.la[ls]["cmds"]["fun"]["cry"]["variable2"]))
+        .setColor("RED")
+        .setDescription(eval(client.la[ls]["cmds"]["fun"]["cry"]["variable3"]))
+      ]}).catch(() => {})
+      
+      //get the memer image
+      client.memer.cry(text).then(image => {
+        //make an attachment
+        var attachment = new MessageAttachment(image, "cry.png");
+        //delete old message
+        tempmsg.delete();
+        //send new Message
+        message.reply({embeds : [tempmsg.embeds[0]
+          .setAuthor(`Meme for: ${message.author.tag}`, message.author.displayAvatarURL())
+          .setColor(es.color)
+          .setImage("attachment://cry.png")
+        ], files : [attachment]}).catch(() => {})
+      })
+      
+  }
+}
+/**
+ * @INFO
+ * Bot Coded by Tomato#6966 | https://discord.gg/milrato
+ * @INFO
+ * Work for Milrato Development | https://milrato.eu
+ * @INFO
+ * Please mention him / Milrato Development, when using this Code!
+ * @INFO
+ */
---
+++ commands/🕹️ Fun/kill.js
@@ -0,0 +1,60 @@
+const Discord = require("discord.js");
+const {MessageEmbed, MessageAttachment} = require("discord.js");
+const config = require(`${process.cwd()}/botconfig/config.json`);
+const canvacord = require("canvacord");
+var ee = require(`${process.cwd()}/botconfig/embed.json`);
+const request = require("request");
+const emoji = require(`${process.cwd()}/botconfig/emojis.json`);
+const path = require("path");
+const { GetUser } = require(`${process.cwd()}/handlers/functions`)
+module.exports = {
+  name: path.parse(__filename).name,
+  category: "🕹️ Fun",
+  usage: `${path.parse(__filename).name} [@User]`,
+  type: "user",
+  description: "*Image cmd in the style:* " + path.parse(__filename).name,
+  run: async (client, message, args, cmduser, text, prefix) => {
+    
+    let es = client.settings.get(message.guild.id, "embed");let ls = client.settings.get(message.guild.id, "language")
+        if(!client.settings.get(message.guild.id, "FUN")){
+          return message.reply({embeds : [new MessageEmbed()
+            .setColor(es.wrongcolor)
+            .setFooter(client.getFooter(es))
+            .setTitle(client.la[ls].common.disabled.title)
+            .setDescription(require(`${process.cwd()}/handlers/functions`).handlemsg(client.la[ls].common.disabled.description, {prefix: prefix}))
+          ]});
+        }
+    try {
+      //find the USER
+      var user;
+      try{
+        user = await GetUser(message, args)
+      }catch (e){
+        return message.reply({content: String('```' + e.message ? String(e.message).substr(0, 1900) : String(e) + '```')})
+      }
+      message.reply({embeds : [new MessageEmbed()
+        .setColor(es.color)
+        .setFooter(client.getFooter(es))
+        .setDescription(eval(client.la[ls]["cmds"]["fun"]["kill"]["variable1"]))
+        .setImage("https://cdn.zerotwo.dev/SHOOT/028bfc32-c06b-4295-87a5-7ddaef08d5ef.gif")
+      ]});
+    } catch (e) {
+      console.log(String(e.stack).grey.bgRed)
+      return message.reply({embeds : [new MessageEmbed()
+        .setColor(es.wrongcolor)
+        .setFooter(client.getFooter(es))
+        .setTitle(client.la[ls].common.erroroccur)
+        .setDescription(eval(client.la[ls]["cmds"]["fun"]["kill"]["variable2"]))
+      ]});
+    }
+  },
+};
+/**
+ * @INFO
+ * Bot Coded by Tomato#6966 | https://discord.gg/milrato
+ * @INFO
+ * Work for Milrato Development | https://milrato.eu
+ * @INFO
+ * Please mention him / Milrato Development, when using this Code!
+ * @INFO
+ */
