741a4342a8174277769f3a3c985894f166295d9f
Tomato6966 <chris.pre03@gmail.com>
2022-01-23 10:43:45 +0100

    Create keepAlive-Server.js

---
+++ handlers/keepAlive-Server.js
@@ -0,0 +1,6 @@
+module.exports = () => {
+  const express = require('express'), app = express(), port = 3000;
+  app.get('/', (req, res) => res.send('<html><head> <link href="https://fonts.googleapis.com/css?family=Roboto Condensed" rel="stylesheet"> <style>body{font-family: "Roboto Condensed"; font-size: 21px; color: white; background-color: #23272A; margin-left: 5%; margin-top: 2%;}a{color: #5865F2}a:hover{color: #818bff}h1{font-size: 48px;}</style></head><body> <h1>Hosting Active</h1> <p>This Bot is the Repository from <a href="https://github.com/tomato6966/Multipurpose-discord-bot" target="_blank">Clan bot </a> <br/><br/> Check it out and make sure to <b>credit Tomato /<a href="https://discord.gg/milrato"> Milrato Development</a></b> in the Bot, if you want to use this Code<br/><br/> <a href="https://discord.gg/milrato"> <img src="https://discord.com/api/guilds/773668217163218944/widget.png?style=banner2"> </a><br/><br/><i>Make sure to add the HOSTING URL to some sort of <a href="https://uptimerobot.com/">UPTIMER LINK SYSTEM</a></i></p></body></html>'));
+
+  app.listen(port, () => console.log(`24/7 Keepalive Server is online! Make sure to add the Repl.co URL to an Uptimer System, Websystem is listening at http://localhost:${port}`.bgGreen.white));
+};
