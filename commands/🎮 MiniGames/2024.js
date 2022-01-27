d75206fe2f8e24aca02832f7c972ac1a82bdaea1
Tomato6966 <chris.pre03@gmail.com>
2022-01-27 15:04:10 +0100

    Update 2024.js

--- commands/🎮 MiniGames/2024.js
+++ commands/🎮 MiniGames/2024.js
@@ -4,95 +4,63 @@ const {
   MessageActionRow,
   MessageAttachment
 } = require('discord.js')
-function disableButtons(components) {
-  for (let x = 0; x < components.length; x++) {
-    for (let y = 0; y < components[x].components.length; y++) {
-      components[x].components[y].disabled = true;
-    }
-  }
-  return components;
-}
-function move(pos, dir) {
-  switch (dir) {
-    case 'up':
-      return {
-        x: pos.x, y: pos.y - 1
-      }
-      case 'down':
-        return {
-          x: pos.x, y: pos.y + 1
-        }
-        case 'left':
-          return {
-            x: pos.x - 1, y: pos.y
-          }
-          case 'right':
-            return {
-              x: pos.x + 1, y: pos.y
-            }
-  }
-}
-function isInsideBlock(pos, width, height) {
-  return pos.x >= 0 && pos.y >= 0 && pos.x < width && pos.y < height;
-}
-function posEqual(pos1, pos2) {
-  return pos1.x === pos2.x && pos1.y === pos2.y;
-}
-function oppDirection(dir) {
-  switch (dir) {
-    case 'up':
-      return 'down'
-    case 'down':
-      return 'up'
-    case 'left':
-      return 'right'
-    case 'right':
-      return 'left'
-  }
-}
+const apiBase = "https://api.aniketdev.cf"
+
 const chars = '0123456789abcdefghijklmnopqrstuvwxyz';
 const WIDTH = 4;
 const HEIGHT = 4;
+
+const config = require(`${process.cwd()}/botconfig/config.json`);
+var ee = require(`${process.cwd()}/botconfig/embed.json`);
+module.exports = {
+  name: "2048",
+  category: "🎮 MiniGames",
+  description: "Play a game of 2048",
+  usage: "2048 --> Play the Game",
+  type: "buttons",
+  run: async (client, message, args, cmduser, text, prefix) => {
+    let es = client.settings.get(message.guild.id, "embed");
+    let ls = client.settings.get(message.guild.id, "language")
+    if (!client.settings.get(message.guild.id, "MINIGAMES")) {
+      return message.reply(new MessageEmbed()
+        .setColor(es.wrongcolor)
+        .setFooter(client.getFooter(es))
+        .setTitle(client.la[ls].common.disabled.title)
+        .setDescription(require(`${process.cwd()}/handlers/functions`).handlemsg(client.la[ls].common.disabled.description, {
+          prefix: prefix
+        }))
+      );
+    }
+    new TwoZeroFourEight({
+      message: message,
+      embed: {
+        title: '2048',
+        color: es.color,
+        overTitle: "Game Over"
+      },
+    }).startGame();
+
+  }
+}
+
+
 class TwoZeroFourEight {
   constructor(options = {}) {
-    if (!options.message) throw new TypeError('NO_MESSAGE: Please provide a message arguement')
-    if (typeof options.message !== 'object') throw new TypeError('INVALID_MESSAGE: Invalid Discord Message object was provided.')
-    if (!options.slash_command) options.slash_command = false;
-    if (typeof options.slash_command !== 'boolean') throw new TypeError('INVALID_COMMAND_TYPE: Slash command must be a boolean.')
-
-
-    if (!options.embed) options.embed = {};
-    if (!options.embed.title) options.embed.title = '2048';
-    if (typeof options.embed.title !== 'string') throw new TypeError('INVALID_TITLE: Embed Title must be a string.')
-    if (!options.embed.color) options.embed.color = '#5865F2';
-    if (typeof options.embed.color !== 'string') throw new TypeError('INVALID_COLOR: Embed Color must be a string.')
-    if (!options.embed.overTitle) options.embed.overTitle = 'Game Over';
-    if (typeof options.embed.overTitle !== 'string') throw new TypeError('INVALID_OVER_TITLE: Over Title must be a string.')
-
-
-    if (!options.emojis) options.emojis = {};
-    if (!options.emojis.up) options.emojis.up = '⬆️';
-    if (typeof options.emojis.up !== 'string') throw new TypeError('INVALID_EMOJI: Up Emoji must be a string.')
-    if (!options.emojis.left) options.emojis.left = '⬅️';
-    if (typeof options.emojis.left !== 'string') throw new TypeError('INVALID_EMOJI: Up Emoji must be a string.')
-    if (!options.emojis.down) options.emojis.down = '⬇️';
-    if (typeof options.emojis.down !== 'string') throw new TypeError('INVALID_EMOJI: Up Emoji must be a string.')
-    if (!options.emojis.right) options.emojis.right = '➡️';
-    if (typeof options.emojis.right !== 'string') throw new TypeError('INVALID_EMOJI: Up Emoji must be a string.')
-
-    if (!options.othersMessage) options.othersMessage = 'You are not allowed to use buttons for this message!';
-    if (typeof options.othersMessage !== 'string') throw new TypeError('INVALID_OTHERS_MESSAGE: Others Message must be a string.')
-
-
+    options.emojis = {
+      up: '⬆️',
+      down: '⬇️',
+      left: '⬅️',
+      right: '➡️',
+    }
+    options.othersMessage = 'You are not allowed to use buttons for this message!';
     this.options = options;
     this.message = options.message;
-    this.gameBoard = [];
+    this.board = [];
     this.mergedPos = [];
     this.score = 0;
-
     for (let y = 0; y < HEIGHT; y++) {
       for (let x = 0; x < WIDTH; x++) {
-        this.gameBoard[y * WIDTH + x] = 0;
+        this.board[y * WIDTH + x] = 0;
       }
     }
   }
@@ -108,25 +76,20 @@ class TwoZeroFourEight {
         x: parseInt(Math.random() * WIDTH),
         y: parseInt(Math.random() * HEIGHT)
       };
-    } while (this.gameBoard[newPos.y * WIDTH + newPos.x] != 0)
+    } while (this.board[newPos.y * WIDTH + newPos.x] != 0)
 
-    this.gameBoard[newPos.y * WIDTH + newPos.x] = (Math.random() * 100) < 25 ? 2 : 1;
+    this.board[newPos.y * WIDTH + newPos.x] = (Math.random() * 100) < 25 ? 2 : 1;
   }
 
   async sendMessage(content) {
-    if (this.options.slash_command) return await this.message.editReply(content)
     return await this.message.channel.send(content)
   }
 
   getImage() {
-    return new MessageAttachment(`https://api.aniketdev.cf/2048/${this.gameBoard.map(c => chars[c]).join('')}`, 'gameboard.png')
+    return new MessageAttachment(`${apiBase}/2048/${this.board.map(c => chars[c]).join('')}`, 'board.png')
   }
 
   async startGame() {
-    if (this.options.slash_command) {
-      if (!this.message.deferred) await this.message.deferReply({ephemeral: false});
-      this.message.author = this.message.user;
-    }
 
     this.score = 0;
     this.placeNewRandomTile();
@@ -135,11 +98,11 @@ class TwoZeroFourEight {
     const embed = new MessageEmbed()
       .setColor(this.options.embed.color)
       .setTitle(this.options.embed.title)
-      .setImage('attachment://gameboard.png')
+      .setImage('attachment://board.png')
       .addField(this.options.embed.curScore || 'Score', this.score.toString())
-      .setFooter(this.message.author.tag, this.message.author.displayAvatarURL({
+      .setFooter(this.message.client.getFooter(this.message.author.tag + " | Aniket's Api", this.message.author.displayAvatarURL({
         dynamic: true
-      }))
+      })))
 
     const up = new MessageButton().setEmoji(emojis.up).setStyle('PRIMARY').setCustomId('2048_up')
     const left = new MessageButton().setEmoji(emojis.left).setStyle('PRIMARY').setCustomId('2048_left')
@@ -163,25 +126,25 @@ class TwoZeroFourEight {
       idle: 60000
     })
 
-    collector.on('collect', async btn => {
-      if (btn.user.id !== this.message.author.id) {
-        if (this.options.othersMessage == 'false') return await btn.deferUpdate();
-        return btn.reply({
+    collector.on('collect', async buttonInteraction => {
+      if (buttonInteraction.user.id !== this.message.author.id) {
+        if (this.options.othersMessage == 'false') return await buttonInteraction.deferUpdate();
+        return buttonInteraction.reply({
           content: this.options.othersMessage.replace('{author}', this.message.author.tag),
           ephemeral: true
         })
       }
 
-      await btn.deferUpdate();
+      await buttonInteraction.deferUpdate();
       let moved = false;
       this.mergedPos = [];
-      if (btn.customId === '2048_left') {
+      if (buttonInteraction.customId === '2048_left') {
         moved = this.shiftLeft()
-      } else if (btn.customId === '2048_right') {
+      } else if (buttonInteraction.customId === '2048_right') {
         moved = this.shiftRight()
-      } else if (btn.customId === '2048_up') {
+      } else if (buttonInteraction.customId === '2048_up') {
         moved = this.shiftUp()
-      } else if (btn.customId === '2048_down') {
+      } else if (buttonInteraction.customId === '2048_down') {
         moved = this.shiftDown()
       }
 
@@ -196,11 +159,11 @@ class TwoZeroFourEight {
         const editEmbed = new MessageEmbed()
           .setColor(this.options.embed.color)
           .setTitle(this.options.embed.title)
-          .setImage('attachment://gameboard.png')
+          .setImage('attachment://board.png')
           .addField(this.options.embed.curScore || 'Score', this.score.toString())
-          .setFooter(this.message.author.tag, this.message.author.displayAvatarURL({
+          .setFooter(this.message.client.getFooter(this.message.author.tag + " | Aniket's Api", this.message.author.displayAvatarURL({
             dynamic: true
-          }))
+          })))
 
         msg.edit({
           embeds: [editEmbed],
@@ -218,16 +181,16 @@ class TwoZeroFourEight {
   }
 
   async gameOver(msg) {
-    const overTitle = this.gameBoard.includes('b') ? this.options.embed.winTitle || 'Win!' : this.options.embed.overTitle;
+    const overTitle = this.board.includes('b') ? this.options.embed.winTitle || 'Win!' : this.options.embed.overTitle;
 
     const editEmbed = new MessageEmbed()
       .setColor(this.options.embed.color)
       .setTitle(this.options.embed.title)
-      .setImage('attachment://gameboard.png')
+      .setImage('attachment://board.png')
       .addField(overTitle, (this.options.embed.totalScore || '**Score:** ') + this.score)
-      .setFooter(this.message.author.tag, this.message.author.displayAvatarURL({
+      .setFooter(this.message.client.getFooter(this.message.author.tag + " | Aniket's Api", this.message.author.displayAvatarURL({
         dynamic: true
-      }))
+      })))
 
     msg.edit({
       embeds: [editEmbed],
@@ -241,7 +204,7 @@ class TwoZeroFourEight {
   isBoardFull() {
     for (let y = 0; y < HEIGHT; y++)
       for (let x = 0; x < WIDTH; x++)
-        if (this.gameBoard[y * WIDTH + x] === 0)
+        if (this.board[y * WIDTH + x] === 0)
           return false;
     return true;
   }
@@ -254,10 +217,10 @@ class TwoZeroFourEight {
           x,
           y
         };
-        const posNum = this.gameBoard[pos.y * WIDTH + pos.x];
+        const posNum = this.board[pos.y * WIDTH + pos.x];
         ['down', 'left', 'right', 'up'].forEach(dir => {
           const newPos = move(pos, dir);
-          if (isInsideBlock(newPos, WIDTH, HEIGHT) && (this.gameBoard[newPos.y * WIDTH + newPos.x] === 0 || this.gameBoard[newPos.y * WIDTH + newPos.x] === posNum))
+          if (isInsideBlock(newPos, WIDTH, HEIGHT) && (this.board[newPos.y * WIDTH + newPos.x] === 0 || this.board[newPos.y * WIDTH + newPos.x] === posNum))
             numMoves++;
         });
       }
@@ -313,7 +276,7 @@ class TwoZeroFourEight {
 
   shift(pos, dir) {
     let moved = false;
-    const movingNum = this.gameBoard[pos.y * WIDTH + pos.x];
+    const movingNum = this.board[pos.y * WIDTH + pos.x];
 
     if (movingNum === 0) {
       return false
@@ -323,21 +286,21 @@ class TwoZeroFourEight {
     let set = false;
     while (!set) {
       moveTo = move(moveTo, dir);
-      const moveToNum = this.gameBoard[moveTo.y * WIDTH + moveTo.x];
+      const moveToNum = this.board[moveTo.y * WIDTH + moveTo.x];
       if (!isInsideBlock(moveTo, WIDTH, HEIGHT) || (moveToNum !== 0 && moveToNum !== movingNum) || !!this.mergedPos.find(p => p.x === moveTo.x && p.y === moveTo.y)) {
         const oppDir = oppDirection(dir);
         const moveBack = move(moveTo, oppDir);
         if (!posEqual(moveBack, pos)) {
-          this.gameBoard[pos.y * WIDTH + pos.x] = 0;
-          this.gameBoard[moveBack.y * WIDTH + moveBack.x] = movingNum;
+          this.board[pos.y * WIDTH + pos.x] = 0;
+          this.board[moveBack.y * WIDTH + moveBack.x] = movingNum;
           moved = true;
         }
         set = true;
       } else if (moveToNum === movingNum) {
         moved = true;
-        this.gameBoard[moveTo.y * WIDTH + moveTo.x] += 1;
-        this.score += Math.floor(Math.pow(this.gameBoard[moveTo.y * WIDTH + moveTo.x], 2));
-        this.gameBoard[pos.y * WIDTH + pos.x] = 0;
+        this.board[moveTo.y * WIDTH + moveTo.x] += 1;
+        this.score += Math.floor(Math.pow(this.board[moveTo.y * WIDTH + moveTo.x], 2));
+        this.board[pos.y * WIDTH + pos.x] = 0;
         set = true;
         this.mergedPos.push(moveTo)
       }
@@ -346,36 +309,50 @@ class TwoZeroFourEight {
   }
 
 }
-const config = require(`${process.cwd()}/botconfig/config.json`);
-var ee = require(`${process.cwd()}/botconfig/embed.json`);
-module.exports = {
-  name: "2048",
-  category: "🎮 MiniGames",
-  description: "Play a game of 2048",
-  usage: "2048 --> Play the Game",
-  type: "buttons",
-  run: async (client, message, args, cmduser, text, prefix) => {
-    let es = client.settings.get(message.guild.id, "embed");
-    let ls = client.settings.get(message.guild.id, "language")
-    if (!client.settings.get(message.guild.id, "MINIGAMES")) {
-      return message.reply(new MessageEmbed()
-        .setColor(es.wrongcolor)
-        .setFooter(client.getFooter(es))
-        .setTitle(client.la[ls].common.disabled.title)
-        .setDescription(require(`${process.cwd()}/handlers/functions`).handlemsg(client.la[ls].common.disabled.description, {
-          prefix: prefix
-        }))
-      );
-    }
-    new TwoZeroFourEight({
-      message: message,
-      slash_command: false,
-      embed: {
-        title: '2048',
-        color: es.color,
-        overTitle: "Game Over"
-      },
-    }).startGame();
 
+function disableButtons(components) {
+  for (let x = 0; x < components.length; x++) {
+    for (let y = 0; y < components[x].components.length; y++) {
+      components[x].components[y].disabled = true;
+    }
+  }
+  return components;
+}
+function move(pos, dir) {
+  switch (dir) {
+    case 'up':
+      return {
+        x: pos.x, y: pos.y - 1
+      }
+      case 'down':
+        return {
+          x: pos.x, y: pos.y + 1
+        }
+        case 'left':
+          return {
+            x: pos.x - 1, y: pos.y
+          }
+          case 'right':
+            return {
+              x: pos.x + 1, y: pos.y
+            }
+  }
+}
+function isInsideBlock(pos, width, height) {
+  return pos.x >= 0 && pos.y >= 0 && pos.x < width && pos.y < height;
+}
+function posEqual(pos1, pos2) {
+  return pos1.x === pos2.x && pos1.y === pos2.y;
+}
+function oppDirection(dir) {
+  switch (dir) {
+    case 'up':
+      return 'down'
+    case 'down':
+      return 'up'
+    case 'left':
+      return 'right'
+    case 'right':
+      return 'left'
   }
 }
