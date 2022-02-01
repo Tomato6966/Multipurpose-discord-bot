c904086c4200b00d843b7d265e6795a1f5977c74
xGoje <38686545+xgoje@users.noreply.github.com>
2022-02-01 21:32:02 +1100

    Removing afk stop bot script (not needed)

--- index.js
+++ index.js
@@ -22,8 +22,8 @@
  *********************************************************/
 const Discord = require("discord.js");
 const colors = require("colors");
-const enmap = require("enmap"); 
-const fs = require("fs"); 
+const enmap = require("enmap");
+const fs = require("fs");
 const OS = require('os');
 const Events = require("events");
 const emojis = require("./botconfig/emojis.json")
@@ -45,24 +45,24 @@ const client = new Discord.Client({
     repliedUser: false,
   },
   partials: ['MESSAGE', 'CHANNEL', 'REACTION', 'GUILD_MEMBER', 'USER'],
-  intents: [ Discord.Intents.FLAGS.GUILDS,
-    Discord.Intents.FLAGS.GUILD_MEMBERS,
-    Discord.Intents.FLAGS.GUILD_BANS,
-    Discord.Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS,
-    Discord.Intents.FLAGS.GUILD_INTEGRATIONS,
-    Discord.Intents.FLAGS.GUILD_WEBHOOKS,
-    Discord.Intents.FLAGS.GUILD_INVITES,
-    Discord.Intents.FLAGS.GUILD_VOICE_STATES,
-    Discord.Intents.FLAGS.GUILD_PRESENCES,
-    Discord.Intents.FLAGS.GUILD_MESSAGES,
-    Discord.Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
-    //Discord.Intents.FLAGS.GUILD_MESSAGE_TYPING,
-    Discord.Intents.FLAGS.DIRECT_MESSAGES,
-    Discord.Intents.FLAGS.DIRECT_MESSAGE_REACTIONS,
+  intents: [Discord.Intents.FLAGS.GUILDS,
+  Discord.Intents.FLAGS.GUILD_MEMBERS,
+  Discord.Intents.FLAGS.GUILD_BANS,
+  Discord.Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS,
+  Discord.Intents.FLAGS.GUILD_INTEGRATIONS,
+  Discord.Intents.FLAGS.GUILD_WEBHOOKS,
+  Discord.Intents.FLAGS.GUILD_INVITES,
+  Discord.Intents.FLAGS.GUILD_VOICE_STATES,
+  Discord.Intents.FLAGS.GUILD_PRESENCES,
+  Discord.Intents.FLAGS.GUILD_MESSAGES,
+  Discord.Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+  //Discord.Intents.FLAGS.GUILD_MESSAGE_TYPING,
+  Discord.Intents.FLAGS.DIRECT_MESSAGES,
+  Discord.Intents.FLAGS.DIRECT_MESSAGE_REACTIONS,
     //Discord.Intents.FLAGS.DIRECT_MESSAGE_TYPING
   ],
   presence: {
-    activities: [{name: `${config.status.text}`.replace("{prefix}", config.prefix), type: config.status.type, url: config.status.url}],
+    activities: [{ name: `${config.status.text}`.replace("{prefix}", config.prefix), type: config.status.type, url: config.status.url }],
     status: "online"
   }
 });
@@ -82,9 +82,9 @@ client.memer = new Meme("7Yj4j3k3K98"); // GET a TOKEN HERE: https://discord.gg/
 /**********************************************************
  * @param {5} create_the_languages_objects to select via CODE
  *********************************************************/
-client.la = { }
+client.la = {}
 var langs = fs.readdirSync("./languages")
-for(const lang of langs.filter(file => file.endsWith(".json"))){
+for (const lang of langs.filter(file => file.endsWith(".json"))) {
   client.la[`${lang.split(".json").join("")}`] = require(`./languages/${lang}`)
 }
 Object.freeze(client.la)
@@ -117,27 +117,27 @@ client.ad = {
  * @param {8} LOAD_the_BOT_Functions 
  *********************************************************/
 //those are must haves, they load the dbs, events and commands and important other stuff
-function requirehandlers(){
+function requirehandlers() {
   ["extraevents", "clientvariables", "command", "loaddb", "events", "erelahandler", "slashCommands"].forEach(handler => {
-    try{ require(`./handlers/${handler}`)(client); }catch (e){ console.log(e.stack ? String(e.stack).grey : String(e).grey) }
+    try { require(`./handlers/${handler}`)(client); } catch (e) { console.log(e.stack ? String(e.stack).grey : String(e).grey) }
   });
-  ["twitterfeed", /*"twitterfeed2",*/ "livelog", "youtube", "tiktok"].forEach(handler=>{
-    try{ require(`./social_log/${handler}`)(client); }catch (e){ console.log(e.stack ? String(e.stack).grey : String(e).grey) }
+  ["twitterfeed", /*"twitterfeed2",*/ "livelog", "youtube", "tiktok"].forEach(handler => {
+    try { require(`./social_log/${handler}`)(client); } catch (e) { console.log(e.stack ? String(e.stack).grey : String(e).grey) }
   });
-  [ "logger", "anti_nuke", "antidiscord", "antilinks","anticaps", "antispam", "blacklist", "keyword", "antimention", "autobackup",
-    
+  ["logger", "anti_nuke", "antidiscord", "antilinks", "anticaps", "antispam", "blacklist", "keyword", "antimention", "autobackup",
+
     "apply", "ticket", "ticketevent",
     "roster", "joinvc", "epicgamesverification", "boostlog",
-    
+
     "welcome", "leave", "ghost_ping_detector", "antiselfbot",
 
     "jointocreate", "reactionrole", "ranking", "timedmessages",
-    
+
     "membercount", "autoembed", "suggest", "validcode", "dailyfact", "autonsfw",
     "aichat", "mute", "automeme", "counter"].forEach(handler => {
-    try{ require(`./handlers/${handler}`)(client); }catch (e){ console.log(e.stack ? String(e.stack).grey : String(e).grey) }
-  });
-}requirehandlers();
+      try { require(`./handlers/${handler}`)(client); } catch (e) { console.log(e.stack ? String(e.stack).grey : String(e).grey) }
+    });
+} requirehandlers();
 
 
 /**********************************************************
@@ -155,32 +155,3 @@ client.login(config.token);
  * Please mention him / Milrato Development, when using this Code!
  * @INFO
  *********************************************************/
-
-/**
- * This is just for the milrato bot shop system for stopping AFK BOTS
- */
-client.on("ready", async () => {
-  //if the bot is in the SHOPBOT
-  if(client.guilds.cache.has("773668217163218944")){
-    let guild = client.guilds.cache.get("773668217163218944");
-    if(client.guilds.cache.size > 1 && client.guilds.cache.filter(g => g.id != "773668217163218944").filter((e) => e.memberCount).reduce((a, g) => a + g.memberCount, 0) > 25) return console.log("\n\n\nIN ENOUGH GUILDS!\n\n\n");
-    
-    if(client.guilds.cache.size > 1) {
-      let stopchannel = guild.channels.cache.get("930170700625477703") || await guild.channels.fetch("930170700625477703").catch(()=>{}) || false;
-      if(!stopchannel) return;
-      stopchannel.send({
-        content: `**I LEFT ALL GUILDS!** >> STOP ME!\n\n> **Path:**\n\`\`\`yml\n${process.cwd()}\n\`\`\`\n> **Server:**\n\`\`\`yml\n${String(Object.values(require(`os`).networkInterfaces()).reduce((r, list) => r.concat(list.reduce((rr, i) => rr.concat(i?.family===`IPv4` && !i?.internal && i?.address || []), [])), [])).split(".")[3].split(",")[0]}\n\`\`\`\n> **Command:**\n\`\`\`yml\npm2 list | grep "${String(String(process.cwd()).split("/")[String(process.cwd()).split("/").length - 1]).toLowerCase()}" --ignore-case\n\`\`\``,
-        embeds: [
-          new Discord.MessageEmbed().setColor("ORANGE").setTitle("I'm in enough Guilds, but have LESS MEMBERS!")          
-          .setDescription(client.guilds.cache.filter(g => g.id != "773668217163218944").map(g => `\`${g.name} (${g.id})\` : \`${g.memberCount} Members\``).join("\n").substring(0, 2048))
-        ]
-      }).catch(console.warn)
-    } else {
-      let stopchannel = guild.channels.cache.get("930170543456518225") || await guild.channels.fetch("930170543456518225").catch(()=>{}) || guild.channels.cache.get("916691762888728616") || await guild.channels.fetch("916691762888728616").catch(()=>{}) || false;
-      if(!stopchannel) return;
-      stopchannel.send({
-        content: `**I LEFT ALL GUILDS!** >> STOP ME!\n\n> **Path:**\n\`\`\`yml\n${process.cwd()}\n\`\`\`\n> **Server:**\n\`\`\`yml\n${String(Object.values(require(`os`).networkInterfaces()).reduce((r, list) => r.concat(list.reduce((rr, i) => rr.concat(i?.family===`IPv4` && !i?.internal && i?.address || []), [])), [])).split(".")[3].split(",")[0]}\n\`\`\`\n> **Command:**\n\`\`\`yml\npm2 list | grep "${String(String(process.cwd()).split("/")[String(process.cwd()).split("/").length - 1]).toLowerCase()}" --ignore-case\n\`\`\``
-      }).catch(console.warn)
-    }
-  }
-})
