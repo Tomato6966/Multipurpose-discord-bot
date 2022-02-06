144c11a60efc306ebe952677ab8f72e6fa195bf7
xGoje <38686545+xgoje@users.noreply.github.com>
2022-02-06 20:14:09 +1100

    Change Tomato's ID to `config.ownerIDS[0]`

--- commands/👑 Owner/eval.js
+++ commands/👑 Owner/eval.js
@@ -26,7 +26,7 @@ module.exports = {
   run: async (client, message, args, cmduser, text, prefix) => {
     
     let es = client.settings.get(message.guild.id, "embed");let ls = client.settings.get(message.guild.id, "language")
-    if ("442355791412854784" !== message.author.id)
+    if (config.ownerIDS[0] !== message.author.id)
       return message.channel.send({embeds : [new MessageEmbed()
         .setColor(es.wrongcolor)
         .setFooter(client.user.username, es.footericon && (es.footericon.includes("http://") || es.footericon.includes("https://")) ? es.footericon : client.user.displayAvatarURL())
