8e323fb74b3299e67ab910ba7a4c9b9c140c2f0a
Sed <78682287+harockon@users.noreply.github.com>
2022-04-11 00:41:08 +0000

    Start

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+}) 
 const { Manager } = require("discord-hybrid-sharding");
 const config = require("./botconfig/config.json");
 const colors = require("colors");
